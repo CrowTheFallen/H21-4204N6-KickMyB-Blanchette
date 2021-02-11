d74689262be54240de7ef98cb4548ca98673a036
Alex Blanchette <bigue1994@hotmail.com>
2021-02-11 17:08:21 -0500

    FCT Page de création
    Ajoute la fonctionnalité de création d’une tâche

--- app/src/main/java/com/example/kickmyb/CreationActivity.java
+++ app/src/main/java/com/example/kickmyb/CreationActivity.java
@@ -1,5 +1,6 @@
 package com.example.kickmyb;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
@@ -16,5 +17,13 @@ public class CreationActivity extends AppCompatActivity {
         binding = ActivityCreationBinding.inflate(getLayoutInflater());
         View view = binding.getRoot();
         setContentView(view);
+
+        binding.button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(CreationActivity.this,HomeActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/kickmyb/HomeActivity.java
+++ app/src/main/java/com/example/kickmyb/HomeActivity.java
@@ -1,5 +1,6 @@
 package com.example.kickmyb;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
@@ -25,6 +26,14 @@ public class HomeActivity extends AppCompatActivity {
         binding = ActivityHomeBinding.inflate(getLayoutInflater());
         View view = binding.getRoot();
         setContentView(view);
+
+        binding.button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(HomeActivity.this,CreationActivity.class);
+                startActivity(intent);
+            }
+        });
 
         this.initRecycler();
         this.remplirRecycler();
